faf84a1c864f93b22bb528bdceb7d5ce74d92f14
吴昭琪 <1944169531@qq.com>
2018-03-29 17:31:52 +0800

    创建服务器

---
+++ http.js
@@ -0,0 +1,12 @@
+//创建服务器
+var http=require('http');
+var server=http.createServer();
+server.listen(3000,function () {
+    console.log("服务器已在3000端口启动")
+});
+//监听请求
+server.on("request",function (request,response) {
+    console.log(request.url);
+    response.write('111');
+    response.end();
+})
