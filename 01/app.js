d57ba0b4793ddc7c887af32360c4fd9ed5a23edc
吴昭琪 <1944169531@qq.com>
2018-04-18 17:59:47 +0800

    01项目-实现首页模板/404模板加载

--- 01/app.js
+++ 01/app.js
@@ -2,8 +2,32 @@
 
 var http=require('http');
 var server=http.createServer();
+var fs=require('fs');
 server.listen(3000,function () {
     console.log("服务器已在3000端口启动")
 });
-
+//监听请求
+server.on("request",function (req,res) {
+    res.setHeader('Content-type','text/html;charset=utf-8');
+    var url=req.url;
+    console.log(url)
+    if(url=='/'){
+        fs.readFile('./public/views/index.html',function(err,data){
+            if(err){
+                return res.end('404 Not Found');
+            }
+            data=data.toString();
+            //console.log(data)
+            res.end(data)
+        })
+    }else{
+        fs.readFile('./public/views/404.html',function(err,data){
+            if(err){
+                return res.end('404 Not Found');
+            }
+            data=data.toString();
+            res.end(data)
+        })
+    }
+});
 
